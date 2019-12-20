ceacfd6811bf8c4cea6a7a172eabe9fae5a003c4
Ross Miglin <rossmiglin@gmail.com>
2019-12-20 00:04:09 -0500

    added knock knock joke

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,21 +11,36 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    //Remember to delete this later on
+    //@State var whosThereIsVisible: Bool = false
     
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-            Button(action: {
-                print("Button pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Welcome to my first app!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                Button(action: {
+                    print("Button pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible){ () -> Alert in
+                    return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                }
+                /*Button(action: {
+                    print("Who's there?")
+                    self.whosThereIsVisible = true
+                }) {
+                    Text("Knock Knock!")
+                }
+                .alert(isPresented: $whosThereIsVisible){ () ->
+                    Alert in
+                    return Alert(title: Text("Who's there?"), message: Text("Sam, Sam Brick. Isn't that a funny name?"), dismissButton: .default(Text("hahaha")))
+                }*/
             }
-            .alert(isPresented: $alertIsVisible){ () -> Alert in
-                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
-            }
+
         }
     }
 }
