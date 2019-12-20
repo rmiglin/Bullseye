ccf719af3fd4119d96dd69431f7e97819fae42fd
Ross Miglin <rossmiglin@gmail.com>
2019-12-20 00:57:15 -0500

    added functionality to slider

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,23 +11,28 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
             VStack {
+                Spacer()
                 
                 //Target row
                 HStack {
                     Text("Put the bullseye as close as you can to:")
                     Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
                 }
+                Spacer()
                 
                 //Slider row
                 HStack {
                     Text("1")
-                    Slider(value: .constant(10))
+                    Slider(value: self.$sliderValue, in: 1...100)
                     Text("100")
                 }
+                Spacer()
+                
                 //Button row
                 Button(action: {
                     print("Button pressed!")
@@ -38,20 +43,25 @@ struct ContentView: View {
                 .alert(isPresented: $alertIsVisible){ () -> Alert in
                     return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up"), dismissButton: .default(Text("Awesome!")))
                 }
+                Spacer()
                 
                 //Score row
                 HStack{
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                         Text("Start over")
                     }
+                    Spacer()
                     Text("Score:")
                     Text("999999")
+                    Spacer()
                     Text("Round:")
                     Text("999")
+                    Spacer()
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                         Text("info")
                     }
                 }
+                .padding(.bottom, 20)
             }
         }
     }
